c0bbca5c44acd350ad89c4a7a66e8587a7327add
nortrix0 <minecrafternortrix@gmail.com>
2023-03-29 15:18:56 -0500

    Shrinkify

--- GoL_C++/GoL_C++.cpp
+++ GoL_C++/GoL_C++.cpp
@@ -1,31 +1,27 @@
 ﻿// GoL_C++.cpp : This file contains the 'main' function. Program execution begins and ends there.
-#include <chrono>
-#include <thread>
+#include <windows.h>
+#include <stdio.h>
 const int width = 118;//To have a single char represent a cell multiply this by 2
 const int height = 62;
 const int chanceOfInitialCell = 10;
 bool cells[height][width];
 bool cellsUpdated[height][width];
-bool gameOver = false;
 const int maxListSize = 10;
-int timesBoardsMatched = 0;
 int timesBoardsCanMatch = 50;
-char lastboards[maxListSize][width*2 * height + height];//To have a single char represent a cell remove the multiply by 2
+char lastboards[maxListSize][width * 2 * height + height];//To have a single char represent a cell remove the multiply by 2
 char board[width * 2 * height + height];//To have a single char represent a cell remove the multiply by 2
-int amountInArray = 0;
 int main()
 {
-    printf("\33[?25l");//Turn Cursor Invisible
-    printf("\33[8;063;237t");//Set window size to 1 higher than width and height
     do
     {
-        gameOver = false;
+        bool gameOver = false;
+        int amountInArray = 0;
+        int timesBoardsMatched = 0;
         for (unsigned short i = 0; i < width * height; i++)
             cells[i % height][i / height] = rand() % 100 <= chanceOfInitialCell;
         while (!gameOver) {
-            std::this_thread::sleep_for(std::chrono::milliseconds(50));
+            Sleep(50);
             int boardIndex = 0;
-            memcpy(cellsUpdated, cells, sizeof(cells));
             for (int y = 0; y < height; y++)
                 for (int x = 0; x < width; x++)
                 {
@@ -39,14 +35,12 @@ int main()
                     if (x + 1 == width)
                         board[boardIndex++] = '\n';
                 }
-            printf("\33[;H");//Set Cursor Position to Top
-            printf("%s", board);
+            printf("\33[?25l\33[8;%i;%it\33[;H%s", (height + 1), (width * 2 + 1), board);//Turn Cursor Invisible and Set height and width, Set Cursor Position to Top and Print board
             bool matchFound = false;
             for (size_t i = 0; i < maxListSize; i++)
                 if (memcmp(lastboards[i], board, sizeof(board)) == 0)
                     matchFound = true;
-            timesBoardsMatched = matchFound ? timesBoardsMatched + 1 : 0;
-            if (timesBoardsMatched >= timesBoardsCanMatch) gameOver = true;
+            gameOver = matchFound ? timesBoardsMatched++ : 0 >= timesBoardsCanMatch;
             memcpy(lastboards[(amountInArray = amountInArray++ % maxListSize)], board, sizeof(board));
             memcpy(cells, cellsUpdated, sizeof(cells));
         }
