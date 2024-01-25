7e130f16d9b4ea58862cfaea854ab2808403804a
nortrix0 <minecrafternortrix@gmail.com>
2024-01-25 09:57:10 -0600

    Push 1/25/24

--- GoL_C++/GoL_C++.cpp
+++ GoL_C++/GoL_C++.cpp
@@ -1,22 +1,23 @@
 ﻿// GoL_C++.cpp : This file contains the 'main' function. Program execution begins and ends there.
 #include <windows.h>
 #include <stdio.h>
-const int width = 118;//To have a single char represent a cell multiply this by 2
-const int height = 62;
-const int chanceOfInitialCell = 10;
+const int width = 210;
+const int height = 126;
+SHORT fontSize = 1;
+int chanceOfInitialCell = 10;
 bool cells[height][width];
 bool cellsUpdated[height][width];
 const int maxListSize = 10;
 int timesBoardsCanMatch = 50;
-char lastboards[maxListSize][width * 2 * height + height];//To have a single char represent a cell remove the multiply by 2
-char board[width * 2 * height + height];//To have a single char represent a cell remove the multiply by 2
+char lastboards[maxListSize][width * height + height];
+char board[width * height + height];
 int main()
 {
+    SetCurrentConsoleFontEx(GetStdHandle(STD_OUTPUT_HANDLE), FALSE, new CONSOLE_FONT_INFOEX{ sizeof(CONSOLE_FONT_INFOEX), 0, { fontSize,fontSize } });
     do
     {
         bool gameOver = false;
-        int amountInArray = 0;
-        int timesBoardsMatched = 0;
+        int amountInArray = 0, timesBoardsMatched = 0;
         for (unsigned short i = 0; i < width * height; i++)
             cells[i % height][i / height] = rand() % 100 <= chanceOfInitialCell;
         while (!gameOver) {
@@ -30,12 +31,11 @@ int main()
                         for (int nx = (x - 1 + width) % width; nx != (x + 2 + width) % width; nx = (nx + 1) % width)
                             if (!(x == nx && y == ny)) neighborsAlive += cells[ny][nx];
                     cellsUpdated[y][x] = neighborsAlive == 3 || (cells[y][x] && neighborsAlive == 2);
-                    board[boardIndex++] = cellsUpdated[y][x] ? 219 : ' ';//To have a single char as a cell replace 219 with 'O'
-                    board[boardIndex++] = cellsUpdated[y][x] ? 219 : ' ';//To have a single char as a cell remove this line
+                    board[boardIndex++] = cellsUpdated[y][x] ? 219 : ' ';//You can replace 219 with 'O'
                     if (x + 1 == width)
                         board[boardIndex++] = '\n';
                 }
-            printf("\33[?25l\33[8;%i;%it\33[;H%s", (height + 1), (width * 2 + 1), board);//Turn Cursor Invisible and Set height and width, Set Cursor Position to Top and Print board
+            printf("\33[?25l\33[8;%i;%it\33[;H%s", (height + 1), (width), board);//Turn Cursor Invisible and Set height and width, Set Cursor Position to Top and Print board
             bool matchFound = false;
             for (size_t i = 0; i < maxListSize; i++)
                 if (memcmp(lastboards[i], board, sizeof(board)) == 0)
